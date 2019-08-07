854d83108a8dc887fe095c7db4dd3f5614243b99
HardCoreWare <bernardocodu@gmail.com>
2019-08-06 19:18:52 -0500

    acotando datos en base a lo solicitado

--- app/src/Modules/Pac.php
+++ app/src/Modules/Pac.php
@@ -9,11 +9,28 @@ class Pac extends Connection{
     public function index(){
 
         $query=
-        "SELECT 
-        CAST(NET_AMOUNT AS FLOAT64) AS NET_AMOUNT, TRX_CODE, BUSINESS_DATE, TC_SUBGROUP, TC_GROUP, BILL_NO, FISCAL_BILL_NO, TRAN_ACTION_ID, ROOM, TAX_INCLUSIVE_YN, CAST(SUBSTR(BUSINESS_DATE,6,2)AS INT64) AS MONTH 
-        FROM 
+        "SELECT
+        CAST(NET_AMOUNT AS FLOAT64) AS NET_AMOUNT,
+        TRX_CODE,
+        BUSINESS_DATE,
+        TC_SUBGROUP,
+        TC_GROUP,
+        BILL_NO,
+        FISCAL_BILL_NO,
+        TRAN_ACTION_ID,
+        ROOM,
+        TAX_INCLUSIVE_YN,
+        CAST(SUBSTR(BUSINESS_DATE,6,2)AS INT64) AS MONTH
+      FROM
         `pit-analytics-2019.PIT_SISTEMAS.Pac_2018_Lte`
-        ORDER BY BUSINESS_DATE";
+        WHERE
+          TC_GROUP IN (SELECT DISTINCT(TC_GROUP) FROM `pit-analytics-2019.PIT_SISTEMAS.PAC_GROUP` ORDER BY TC_GROUP)
+          AND
+          TC_SUBGROUP IN (SELECT DISTINCT(TC_SUBGROUP) FROM `pit-analytics-2019.PIT_SISTEMAS.PAC_GROUP` ORDER BY TC_SUBGROUP)
+          AND
+          TRX_CODE IN (SELECT DISTINCT(TRX_CODE) FROM `pit-analytics-2019.PIT_SISTEMAS.PAC_GROUP` ORDER BY TRX_CODE)
+      ORDER BY
+        BUSINESS_DATE";
         return $this->bigquery->query($query);
 
     }
